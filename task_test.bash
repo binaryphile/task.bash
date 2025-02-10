ff2cc2d07bed53427d6e1f50af690ef464f9941e
Ted Lilley <ted.lilley@digi.com>
2025-02-10 11:05:45 -0500

    add 128 return code to test setup errors

--- task_test.bash
+++ task_test.bash
@@ -5,13 +5,13 @@ source ./task.bash
 test_task.curl() {
   ## arrange
 
-  subject=${FUNCNAME#test_}
+  command=${FUNCNAME#test_}
   want=$'[begin]		curl http://127.0.0.1:8000/src.txt >dst.txt\n[changed]	curl http://127.0.0.1:8000/src.txt >dst.txt'
 
   # temporary directory
-  dir=$(t.mktempdir) || return  # fail if can't make dir
+  dir=$(t.mktempdir) || return 128  # fatal if can't make dir
   trapcmd="rm -rf $dir"
-  trap $trapcmd EXIT            # always clean up
+  trap $trapcmd EXIT                # always clean up
   cd $dir
 
   # create the downloadable file
@@ -27,7 +27,7 @@ test_task.curl() {
   # run the command and capture the output and result code
   for duration in 0.1 0.2 0.4; do   # retry
     sleep $duration
-    got=$($subject http://127.0.0.1:8000/src.txt dst.txt 2>&1)
+    got=$($command http://127.0.0.1:8000/src.txt dst.txt 2>&1)
     rc=$?
     (( rc == 0 )) && break
   done
@@ -36,19 +36,19 @@ test_task.curl() {
 
   # assert no error
   (( rc == 0 )) || {
-    echo -e "$subject() error = $rc, want: 0\n$got"
+    echo -e "$command: error = $rc, want: 0\n$got"
     return 1
   }
 
   # assert that the file was downloaded
   [[ $(<src.txt) == $(<dst.txt) ]] || {
-    echo -e "$subject() expected file contents to match.\n$got"
+    echo -e "$command: expected file contents to match.\n$got"
     return 1
   }
 
   # assert that we got the wanted output
   [[ $got == "$want" ]] || {
-    echo -e "$subject() got doesn't match want:\n$(t.diff "$got" "$want")"
+    echo -e "$command: got doesn't match want:\n$(t.diff "$got" "$want")"
     return 1
   }
 }
@@ -57,7 +57,7 @@ test_task.curl() {
 # There are subtests for link creation and when link creation fails.
 # Subtests are run with t.run.
 test_task.ln() {
-  subject=${FUNCNAME#test_}
+  command=${FUNCNAME#test_}
 
   local -A case1=(
     [name]='basic'
@@ -72,7 +72,7 @@ test_task.ln() {
   )
 
   # subtest runs each subtest.
-  # subject is the parent function name.
+  # command is the parent function name.
   # case is expected to be the name of an associative array holding at least the key "name".
   # Each subtest that needs a directory creates it in /tmp.
   subtest() {
@@ -82,21 +82,18 @@ test_task.ln() {
     casename=$2
     eval "$(t.inherit $casename)"
 
-    subject=$1
-    name="    $subject/$name()"
-
     # temporary directory
-    dir=$(t.mktempdir) || return  # fail if can't make dir
-    trap "rm -rf $dir" EXIT       # always clean up
+    dir=$(t.mktempdir) || return 128  # fatal if can't make dir
+    trap "rm -rf $dir" EXIT           # always clean up
     cd $dir
 
-    # set positional args for command
-    eval "set -- $args"
+    command=$1
+    eval "set -- $args"   # set positional args for command
 
     ## act
 
     # run the command and capture the output and result code
-    got=$($subject $* 2>&1)
+    got=$($command $* 2>&1)
     rc=$?
 
     ## assert
@@ -105,32 +102,35 @@ test_task.ln() {
     [[ -v wanterr ]] && {
       (( rc == wanterr )) && return
 
-      echo -e "$name error = $rc, want: $wanterr\n$got"
+      echo -e "    $command/$name: error = $rc, want: $wanterr\n$got"
       return 1
     }
 
     # assert no error
     (( rc == 0 )) || {
-      echo -e "$name error = $rc, want: 0\n$got"
+      echo -e "    $command/$name: error = $rc, want: 0\n$got"
       return 1
     }
 
     # assert that the symlink was made
     [[ -L $2 ]] || {
-      echo -e "$name expected $2 to be symlink\n$got"
+      echo -e "    $command/$name expected $2 to be symlink\n$got"
       return 1
     }
 
     # assert that we got the wanted output
     [[ $got == "$want" ]] || {
-      echo -e "$name got doesn't match want:\n$(t.diff "$got" "$want")"
+      echo -e "    $command/$name got doesn't match want:\n$(t.diff "$got" "$want")"
       return 1
     }
   }
 
   failed=0
   for casename in ${!case@}; do
-    t.run subtest $subject $casename || failed=1
+    t.run subtest $command $casename || {
+      (( $? == 128 )) && return   # fatal
+      failed=1
+    }
   done
 
   return $failed
@@ -141,37 +141,37 @@ test_task.ln() {
 test_task.mkdir() {
   ## arrange
 
-  subject=${FUNCNAME#test_}
+  command=${FUNCNAME#test_}
   want=$'[begin]		mkdir -p mydir\n[changed]	mkdir -p mydir'
 
   # temporary directory
-  dir=$(t.mktempdir) || return  # fail if can't make dir
-  trap "rm -rf $dir" EXIT       # always clean up
+  dir=$(t.mktempdir) || return 128  # fatal if can't make dir
+  trap "rm -rf $dir" EXIT           # always clean up
   cd $dir
 
   ## act
 
   # run the command and capture the output and result code
-  got=$($subject mydir 2>&1)
+  got=$($command mydir 2>&1)
   rc=$?
 
   ## assert
 
   # assert no error
   (( rc == 0 )) || {
-    echo -e "$subject error = $rc, want: 0\n$got"
+    echo -e "$command error = $rc, want: 0\n$got"
     return 1
   }
 
   # assert that the directory was made
   [[ -d mydir ]] || {
-    echo -e "$subject expected directory mydir.\n$got"
+    echo -e "$command expected directory mydir.\n$got"
     return 1
   }
 
   # assert that we got the wanted output
   [[ $got == "$want" ]] || {
-    echo -e "$subject got doesn't match want:\n$(t.diff "$got" "$want")"
+    echo -e "$command got doesn't match want:\n$(t.diff "$got" "$want")"
     return 1
   }
 }
@@ -181,37 +181,37 @@ test_task.mkdir() {
 test_task.git_clone() {
   ## arrange
 
-  subject=${FUNCNAME#test_}
+  command=${FUNCNAME#test_}
   want=$'[begin]		git clone https://github.com/binaryphile/task.bash task.bash\n[changed]	git clone https://github.com/binaryphile/task.bash task.bash'
 
   # temporary directory
-  dir=$(t.mktempdir) || return  # fail if can't make dir
-  trap "rm -rf $dir" EXIT       # always clean up
+  dir=$(t.mktempdir) || return 128  # fatal if can't make dir
+  trap "rm -rf $dir" EXIT           # always clean up
   cd $dir
 
   ## act
 
   # run the command and capture the output and result code
-  got=$($subject https://github.com/binaryphile/task.bash task.bash 2>&1)
+  got=$($command https://github.com/binaryphile/task.bash task.bash 2>&1)
   rc=$?
 
   ## assert
 
   # assert no error
   (( rc == 0 )) || {
-    echo -e "$subject error = $rc, want: 0\n$got"
+    echo -e "$command error = $rc, want: 0\n$got"
     return 1
   }
 
   # assert that the repo was cloned
   [[ -e task.bash/.git ]] || {
-    echo -e "$subject expected .git directory.\n$got"
+    echo -e "$command expected .git directory.\n$got"
     return 1
   }
 
   # assert that we got the wanted output
   [[ $got == "$want" ]] || {
-    echo -e "$subject got doesn't match want:\n$(t.diff "$got" "$want")"
+    echo -e "$command got doesn't match want:\n$(t.diff "$got" "$want")"
     return 1
   }
 }
