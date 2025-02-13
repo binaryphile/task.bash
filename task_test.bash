3873e7f40f77cbf9fa07ad061f0d76b7ae1aeb78
Ted Lilley <ted.lilley@digi.com>
2025-02-13 13:02:27 -0500

    wip

--- task_test.bash
+++ task_test.bash
@@ -3,34 +3,31 @@ source ./task.bash
 # test_task.curl tests whether the curl download task receives a file from a local test http server.
 # It does its work in a directory it creates in /tmp.
 test_task.curl() {
+  command=${FUNCNAME#test_}
+
   ## arrange
 
-  command=${FUNCNAME#test_}
-  want=$'[begin]		curl http://127.0.0.1:8000/src.txt >dst.txt\n[changed]	curl http://127.0.0.1:8000/src.txt >dst.txt'
-
   # temporary directory
-  dir=$(t.mktempdir) || return 128  # fatal if can't make dir
+  dir=$(t.mktempdir) || return 128  # fatal
   trapcmd="rm -rf $dir"
-  trap $trapcmd EXIT                # always clean up
+  trap $trapcmd EXIT  # always clean up
   cd $dir
 
   # create the downloadable file
   echo 'hello there' >src.txt
 
-  # start the http server, redirect stdout so the test doesn't hang
-  pid=$(python3 -m http.server 8000 --bind 127.0.0.1 &>/dev/null & echo $!)
+  pid=$(t.start_http_server) || { echo $pid; return 128; }  # if fatal pid is the error message
   trapcmd="kill $pid >/dev/null; $trapcmd"
   trap $trapcmd EXIT  # always clean up
+
+  want=$'[\E[38;5;220mbegin\E[0m]\t\tdownload src.txt from http://127.0.0.1:8000 as dst.txt
+[\E[38;5;208mchanged\E[0m]\tdownload src.txt from http://127.0.0.1:8000 as dst.txt'
 
   ## act
 
   # run the command and capture the output and result code
-  for duration in 0.1 0.2 0.4; do   # retry
-    sleep $duration
-    got=$($command http://127.0.0.1:8000/src.txt dst.txt 2>&1)
-    rc=$?
-    (( rc == 0 )) && break
-  done
+  got=$($command http://127.0.0.1:8000/src.txt dst.txt 2>&1)
+  rc=$?
 
   ## assert
 
@@ -41,7 +38,7 @@ test_task.curl() {
   }
 
   # assert that the file was downloaded
-  [[ $(<src.txt) == $(<dst.txt) ]] || {
+  [[ -f dst.txt && $(<src.txt) == $(<dst.txt) ]] || {
     echo -e "$command: expected file contents to match.\n$got"
     return 1
   }
@@ -62,7 +59,8 @@ test_task.ln() {
   local -A case1=(
     [name]='basic'
     [args]='target.txt link.txt'
-    [want]=$'[begin]		create symlink - target.txt link.txt\n[changed]	create symlink - target.txt link.txt'
+    [want]=$'[\E[38;5;220mbegin\E[0m]\t\tsymlink link.txt to target.txt
+[\E[38;5;208mchanged\E[0m]\tsymlink link.txt to target.txt'
   )
 
   local -A case2=(
@@ -102,25 +100,25 @@ test_task.ln() {
     [[ -v wanterr ]] && {
       (( rc == wanterr )) && return
 
-      echo -e "    $command/$name: error = $rc, want: $wanterr\n$got"
+      echo -e "\t$command: error = $rc, want: $wanterr\n$got"
       return 1
     }
 
     # assert no error
     (( rc == 0 )) || {
-      echo -e "    $command/$name: error = $rc, want: 0\n$got"
+      echo -e "\t$command: error = $rc, want: 0\n$got"
       return 1
     }
 
     # assert that the symlink was made
     [[ -L $2 ]] || {
-      echo -e "    $command/$name expected $2 to be symlink\n$got"
+      echo -e "\t$command: expected $2 to be symlink\n$got"
       return 1
     }
 
     # assert that we got the wanted output
     [[ $got == "$want" ]] || {
-      echo -e "    $command/$name got doesn't match want:\n$(t.diff "$got" "$want")"
+      echo -e "\t$command: got doesn't match want:\n$(t.diff "$got" "$want")"
       return 1
     }
   }
@@ -142,7 +140,8 @@ test_task.mkdir() {
   ## arrange
 
   command=${FUNCNAME#test_}
-  want=$'[begin]		mkdir -p mydir\n[changed]	mkdir -p mydir'
+  want=$'[\E[38;5;220mbegin\E[0m]\t\tmake directory dir
+[\E[38;5;208mchanged\E[0m]\tmake directory dir'
 
   # temporary directory
   dir=$(t.mktempdir) || return 128  # fatal if can't make dir
@@ -182,12 +181,15 @@ test_task.git_clone() {
   ## arrange
 
   command=${FUNCNAME#test_}
-  want=$'[begin]		git clone https://github.com/binaryphile/task.bash task.bash\n[changed]	git clone https://github.com/binaryphile/task.bash task.bash'
+  want=$'[\E[38;5;220mbegin\E[0m]\t\tclone repo https://github.com/binaryphile/task.bash to task.bash
+[\E[38;5;208mchanged\E[0m]\tclone repo https://github.com/binaryphile/task.bash to task.bash'
 
   # temporary directory
   dir=$(t.mktempdir) || return 128  # fatal if can't make dir
   trap "rm -rf $dir" EXIT           # always clean up
   cd $dir
+
+  # TODO: build and clone local repo instead of from github
 
   ## act
 
