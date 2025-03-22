8185cc0cd9613430b40ac3ca2e2aecb3d7ecb199
Ted Lilley <ted.lilley@digi.com>
2025-03-22 13:49:09 -0400

    remove cmd function definition

--- task.bash
+++ task.bash
@@ -2,20 +2,71 @@
 
 # Naming Policy:
 #
-# All function and variable names are camelCased, but they may begin with uppercase letters.
+# All function and variable names are camelCased.
 #
-# Local variable names begin with lowercase letters.
-# Global variable names begin with uppercase letters.
-# Global variable names are namespaced by suffixing them with the randomly-generated letter X.
+# Local variable names begin with lowercase letters, e.g. localVariable.
+#
+# Global variable names begin with uppercase letters, e.g. GlobalVariable.
+# Since this is a library, global variable names are also namespaced by suffixing them with
+# the randomly-generated letter X, e.g. GlobalVariableX.
+# Global variables are not public.  Library consumers should not be aware of them.
+# If users need to interact with them, create accessor functions for the purpose.
 #
 # Private function names begin with lowercase letters.
 # Public function names begin with uppercase letters.
 # Function names are prefixed with "task." (always lowercase) so they are namespaced.
 #
-# Keyword function names are the exception to this.
-# They are all lowercase letters and attempt to be five letters or shorter.
+# Keyword function names are the exception.
+# They are all lowercase letters and should be five letters or shorter.
 
 ## task definition keywords
+
+# cmd runs $cmd after checking that it is not already satisfied and records the result.
+cmd() {
+  local cmd=$1
+
+  [[ $ConditionX != '' ]] && ( eval "$ConditionX" &>/dev/null ) && {
+    OksX[$DescriptionX]=1
+    echo -e "[$(task.t ok)]\t\t$DescriptionX"
+
+    return
+  }
+
+  ! (( ShortRunX || ShowProgressX )) && echo -ne "[$(task.t begin)]\t\t$DescriptionX"
+
+  [[ $RunAsUserX != '' ]] && cmd="sudo -u ${RunAsUserX@Q} bash -c ${cmd@Q}"
+
+  (( ShortRunX )) && {
+    (( ShowProgressX )) || [[ $UnchangedTextX != '' ]] && {
+      echo -e "\r[$(task.t skipping)]\t$DescriptionX"
+
+      return
+    }
+  }
+
+  local rc=0
+  if (( ShowProgressX )); then
+    echo -e "[$(task.t progress)]\t$DescriptionX"
+    OutputX=$(eval "$cmd" 2>&1 | tee /dev/tty) && rc=$? || rc=$?
+  else
+    OutputX=$(eval "$cmd" 2>&1) && rc=$? || rc=$?
+  fi
+
+  if [[ $UnchangedTextX != '' && $OutputX == *"$UnchangedTextX"* ]]; then
+    OksX[$DescriptionX]=1
+    echo -e "\r[$(task.t ok)]\t\t$DescriptionX"
+  elif (( rc == 0 )) && ( eval "$ConditionX" &>/dev/null ); then
+    ChangedsX[$DescriptionX]=1
+    echo -e "\r[$(task.t changed)]\t$DescriptionX"
+  else
+    echo -e "\r[$(task.t failed)]\t$DescriptionX"
+    ! (( ShowProgressX )) && echo -e "[output]\t$DescriptionX\n$OutputX\n"
+    echo 'stopped due to failure'
+    (( rc == 0 )) && echo 'task reported success but condition not met'
+  fi
+
+  return $rc
+}
 
 # desc sets DescriptionX, the task description.
 # Beginning a new task this way also demands reinitialization of the task environment.
@@ -37,61 +88,6 @@ prog() { [[ $1 == on ]] && ShowProgressX=1 || ShowProgressX=0; }
 declare -A OksX=()        # tasks that were already satisfied
 declare -A ChangedsX=()   # tasks that succeeded
 
-# run runs cmd after checking that it is not already satisfied and records the result.
-# DescriptionX must be set externally already.
-run() {
-  [[ $ConditionX != '' ]] && ( eval "$ConditionX" &>/dev/null ) && {
-    OksX[$DescriptionX]=1
-    echo -e "[$(task.t ok)]\t\t$DescriptionX"
-
-    return
-  }
-
-  ! (( ShortRunX || ShowProgressX )) && echo -ne "[$(task.t begin)]\t\t$DescriptionX"
-
-  local command
-  if [[ $RunAsUserX == '' ]]; then
-    command=( cmd )
-  else
-    local remoteCommandString="
-      $(declare -f cmd)
-      cmd
-    "
-    command=( sudo -u "$RunAsUserX" bash -c "$remoteCommandString" )
-  fi
-
-  (( ShortRunX )) && {
-    (( ShowProgressX )) || [[ $UnchangedTextX != '' ]] && {
-      echo -e "\r[$(task.t skipping)]\t$DescriptionX"
-
-      return
-    }
-  }
-
-  local rc=0
-  if (( ShowProgressX )); then
-    echo -e "[$(task.t progress)]\t$DescriptionX"
-    OutputX=$("${command[@]}" 2>&1 | tee /dev/tty) && rc=$? || rc=$?
-  else
-    OutputX=$("${command[@]}" 2>&1) && rc=$? || rc=$?
-  fi
-
-  if [[ $UnchangedTextX != '' && $OutputX == *"$UnchangedTextX"* ]]; then
-    OksX[$DescriptionX]=1
-    echo -e "\r[$(task.t ok)]\t\t$DescriptionX"
-  elif (( rc == 0 )) && ( eval "$ConditionX" &>/dev/null ); then
-    ChangedsX[$DescriptionX]=1
-    echo -e "\r[$(task.t changed)]\t$DescriptionX"
-  else
-    echo -e "\r[$(task.t failed)]\t$DescriptionX"
-    ! (( ShowProgressX )) && echo -e "[output]\t$DescriptionX\n$OutputX\n"
-    echo 'stopped due to failure'
-    (( rc == 0 )) && echo 'task reported success but condition not met'
-  fi
-
-  return $rc
-}
-
 # runas tells the task to run under sudo as user $1
 runas() { RunAsUserX=$1; }
 
@@ -101,18 +97,6 @@ unchg() { UnchangedTextX=$1; }
 
 ## library functions
 
-# task.cmd is the default implementation of cmd. The user calls the default implementation
-# when they define the task using the cmd keyword. The default implementation accepts a
-# command as an argument and redefines cmd to run it.  That then gets invoked by the run
-# keyword.
-task.cmd() {
-  # don't try to make a one-liner with semicolons
-  eval "cmd() {
-    $1
-  }"
-  run
-}
-
 # task.initTaskEnv initializes all relevant settings for a new task.
 task.initTaskEnv() {
   ConditionX=''              # an expression to tell when the task is already satisfied
@@ -120,8 +104,6 @@ task.initTaskEnv() {
   RunAsUserX=''              # the user to sudo with
   ShowProgressX=0            # flag for showing OutputX as the task runs
   UnchangedTextX=''          # text to test for in the OutputX to see task didn't change anything (i.e. is ok)
-
-  cmd() { task.cmd "$@"; }
 }
 
 ShortRunX=0
@@ -178,7 +160,7 @@ task.GitClone() {
 task.Install() {
   local mode=$1 src=$2 dst=$3
 
-  # for paths with spaces
+  # make strings eval-ready
   printf -v src %q "$src"
   printf -v dst %q "$dst"
 
